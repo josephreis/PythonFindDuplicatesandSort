8c99547bc19128d9811f3c811ac111cf8d970a2f
josephreis <reis.joseph.m@gmail.com>
2025-01-27 23:28:14 -0800

    Update FindDuplicatesandSort.py

--- FindDuplicatesandSort.py
+++ FindDuplicatesandSort.py
@@ -1,1 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 27 22:58:39 2025
 
+@author: Joe Reis
+"""
+
+# Framework: Python
+
+import pyperclip
+
+# Clear the console (not directly translatable to Python)
+# Data representing various management and technical skills
+data_string = """Start-ups Management
+Hiring
+Interviewing
+Vendor Partnerships
+Automation
+AUTOMATION
+Start-up Ventures
+Content Strategy
+Search Engine Optimization (SEO)
+SEO Audits
+Wordpress Development
+System Administration
+Vulnerability Research
+Incident Management
+Splunk
+Splunk
+Load Balancing
+Platform Building
+Systems Engineering
+Client Relations
+WordPress
+Log Management
+"""
+
+# Split the data into a list and remove duplicates
+data_list = list(set(data_string.splitlines()))
+print("This is the Data displaying what the duplicates are")
+print(data_list)
+print("\n")
+
+# Sort the data alphabetically
+sorted_data = sorted(data_list)
+print("This is the Data after it removes duplicates")
+print(sorted_data)
+print("\n")
+
+print("This is now the Data after it has been sorted alphabetically")
+print(sorted_data)
+print("\n")
+
+# Store the sorted data in the clipboard
+pyperclip.copy("\n".join(sorted_data))
+print("The data is now stored in your clipboard")
